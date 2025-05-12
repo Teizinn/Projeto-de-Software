a35cac4565ba3c20fe626bef58b087c0ac93669a
Gabriel Teixeira <141375574+Teizinn@users.noreply.github.com>
2025-05-12 08:52:53 -0300

    Add files via upload

---
+++ Projeto-de-Software/SpringThymeleafCRUD/src/main/java/Tei/springthymeleafcrud/repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package tei.springthymeleafcrud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import vicenzo.springthymeleafcrud.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
---
+++ Projeto-de-Software/SpringThymeleafCRUD/src/test/java/Tei/springthymeleafcrud/SpringThymeleafCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package tei.springthymeleafcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
